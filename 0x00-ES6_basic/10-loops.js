16a4f198edecd8dd4e8828df4c5b2e442ad40316
Emmanuel Stanley <emmanuelstanley753@gmail.com>
2024-10-04 22:16:15 +0100

    task: Loops

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,9 @@
+export default function appendToEachArrayValue(array, appendString) {
+  const arr = array;
+  for (const value of array) {
+    const idx = array.indexOf(value);
+    arr[idx] = appendString + value;
+  }
+
+  return arr;
+}
